5c25ce709c415ad1f20616100377e8ca34c1450e
leonidsidorov <synctrax1991@>
2023-09-04 12:59:13 +0300

    update

--- src/components/HomeForm.tsx
+++ src/components/HomeForm.tsx
@@ -7,9 +7,9 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { RouteNames } from '../routes';
 import { Vehicle } from '../models/IVehicle';
-import { apiInstance, imageInstance } from '../axios-instance';
+import { apiInstance} from '../axios-instance';
 
-const HomeForm = () => {
+const HomeForm: React.FC = () => {
     const [vehicles, setVehicles] = useState<Vehicle[]>([]);
     const history = useHistory();
 
@@ -19,7 +19,7 @@ const HomeForm = () => {
 
     useEffect(() => {
         apiInstance
-            .get('vehicles/all')
+            .get('vehicles')
             .then((response) => {
                 const vehiclesData: Vehicle[] = response.data;
                 setVehicles(vehiclesData);
@@ -33,33 +33,38 @@ const HomeForm = () => {
         <Grid container spacing={3}>
             {vehicles
                 .filter((vehicle) => !vehicle.deleted)
-                .map((vehicle) => (
-                    <Grid item xs={12} sm={6} md={4} lg={4} key={vehicle.id}>
-                        <Card>
-                            <CardMedia
-                                component="img"
-                                alt={`${vehicle.make} ${vehicle.model}`}
-                                width="100%"
-                                // Use imageInstance for loading images
-                                image={`http://localhost:8080/api/v1/vehicles/display/${vehicle.images[0]}`}
-                                onClick={() => handleClickOnImage(vehicle.id)}
-                                style={{ objectFit: 'contain' }}
-                                onError={(e) => {
-                                }}
-                            />
-                            <CardContent>
-                                <Typography variant="h6">
-                                    {vehicle.year}, {vehicle.make} {vehicle.model}</Typography>
-                                <Typography variant="body2">
-                                    Owner: {vehicle.sellerUsername}
-                                </Typography>
-                                <Typography variant="body2">
-                                    Expected Price: {vehicle.expectedBid} $
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
+                .map((vehicle, index) => {
+                    const imageUrl = `http://localhost:8080/api/v1/vehicles/preview/${vehicle.images[0]}`;
+
+                    return (
+                        <Grid item xs={12} sm={6} md={4} lg={4} key={vehicle.id}>
+                            <Card>
+                                <CardMedia
+                                    component="img"
+                                    alt={`${vehicle.make} ${vehicle.model}`}
+                                    width="100%"
+                                    src={imageUrl}
+                                    onClick={() => handleClickOnImage(vehicle.id)}
+                                    style={{ objectFit: 'contain' }}
+                                    onError={(e) => {
+                                        // Handle error if needed
+                                    }}
+                                />
+                                <CardContent>
+                                    <Typography variant="h6">
+                                        {vehicle.year}, {vehicle.make} {vehicle.model}
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        Owner: {vehicle.sellerUsername}
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        Expected Price: {vehicle.expectedBid} $
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    );
+                })}
         </Grid>
     );
 };
